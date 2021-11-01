f2097af2ffeaef2bb8bf074c5302cd4b4e1194ab
Onishi1129 <taku.hot.2545@gmail.com>
2021-11-01 03:48:26 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,12 @@ class UsersController < ApplicationController
   end
   
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user), notice: "You have updated user successfully."
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(current_user)
     else
-      render "edit"
+      render :edit
     end
   end
   
