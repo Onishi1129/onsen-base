9bf4534c667bb7697c586cf13b0fd7b920022c58
Onishi1129 <taku.hot.2545@gmail.com>
2021-11-04 17:54:58 +0000

    バリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
    attachment :image
+   validates :onsen_place, presence: true
+   validates :onsen_name, presence: true
+   validates :caption, presence: true, length: {maximum: 300}
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
    has_many :posts, dependent: :destroy
    attachment :profile_image
+   validates :name, uniqueness: true, length: {maximum: 20, minimum: 3}
 end
