7a0f3b513fe8701f65e080a86c8dd46b4648b76b
Onishi1129 <taku.hot.2545@gmail.com>
2021-12-05 18:28:43 +0000

    都道府県修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_063832) do
-
-  create_table "posts", force: :cascade do |t|
-    t.text "onsen_place"
-    t.text "onsen_name"
-    t.string "image_id"
-    t.text "caption"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2021_10_18_072330) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
