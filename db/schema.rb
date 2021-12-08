474d2eb4d0cfce7ee696d588fd1070159fadef26
Onishi1129 <taku.hot.2545@gmail.com>
2021-12-08 16:57:30 +0000

    都道府県プルダウン追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_072330) do
+ActiveRecord::Schema.define(version: 2021_10_19_063832) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "prefectures_select", default: 0, null: false
+    t.text "onsen_place"
+    t.text "onsen_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
