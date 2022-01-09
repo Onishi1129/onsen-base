4fd023c9773492aa12ed0488e66666cdb6ad4902
Onishi1129 <taku.hot.2545@gmail.com>
2022-01-09 11:53:52 +0000

    都道府県js修正済み

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,9 +30,8 @@ $(function() {
         '#user_postal_code'
       ],
       address: {
-        "#user_prefecture_code": "%3", // # 都道府県が入力される
-        "#user_city"           : "%4%5", // # 市区町村と町域が入力される
-        "#user_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+         "#post_prefectures_select": "%3",
+         "#post_onsen_place"       : "%4%5%6%7",
       }
     });
   });
